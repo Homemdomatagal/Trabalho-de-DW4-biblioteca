49ae9835a29e171e09ec72e1e397aa59ca5b40ce
Homemdomatagal <162762580+Homemdomatagal@users.noreply.github.com>
2025-11-12 16:45:28 -0400

    Add initial styles for frontend layout

---
+++ Trabalho DW4/frontend/styles.css
@@ -0,0 +1,79 @@
+body {
+    background-color: rgb(69, 104, 180);
+    display: flex;
+    justify-content: center;
+
+}
+
+.tudo {
+    height: 550px;
+    width: 350px;
+    background-color: white;
+    margin-top: 10%;
+    border-radius: 10px;
+    display: wrap;
+    justify-content: left;
+    box-shadow: rgba(68, 51, 81, 0.212) 10px 10px 10px;
+}
+
+.titulo {
+    padding: 3%;
+    height: 7%;
+    width: 100%;
+    font-size: 25px;
+    font-family: Arial, Helvetica, sans-serif;
+    color: gray;
+}
+
+.caixas {
+    display: wrap;
+    width: 100%;
+    height: 87%;
+}
+
+.caixa {
+    width: 95%;
+    height: 15%;
+    margin-left: 5%;
+    margin-bottom: 5%;
+}
+
+label {
+    font-family: Arial, Helvetica, sans-serif;
+    color: rgb(75, 75, 75);
+    font-size: 19px;
+}
+
+input {
+    height: 40%;
+    width: 80%;
+    border-radius: 10px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 17px;
+}
+
+button {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 17px;
+    border-radius: 10px;
+    height: 40%;
+    width: 60%;
+    background-color: rgb(196, 83, 83);
+    border: 0;
+}
+
+.caixa a{
+    font-family: Arial, Helvetica, sans-serif;
+    color: rgb(75, 75, 75);
+    font-size: 17px;
+    visibility: hidden;
+}
+
+.caixa.error input{
+    border-color: red;
+}
+
+.caixa.error a{
+    color: red;
+    visibility: visible;
+}
